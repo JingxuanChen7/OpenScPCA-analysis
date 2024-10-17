835df75cdd190591a794883d23864f5a36585f8a
Jingxuan Chen <jingxuanchen712@outlook.com>
2024-10-17 20:17:38 +0000

    update scripts

--- analyses/cell-type-wilms-tumor-14/scripts/02_clustering.R
+++ analyses/cell-type-wilms-tumor-14/scripts/02_clustering.R
@@ -15,24 +15,54 @@ library <- "SCPCL000850"
 obj <- SeuratObject::LoadSeuratRds( file.path(path_anal,"scratch","00_preprocessing_rds",paste0(library,".rdsSeurat")) )
 DimPlot(obj, group.by = "cluster")
 
-cluster_df <- calculate_clusters(obj,
-                                 algorithm = "leiden",
-                                 resolution = 0.006, objective_function = "CPM",
-                                 seed = 233)
+# cluster_df <- calculate_clusters(obj,
+#                                  algorithm = "leiden",
+#                                  resolution = 0.006, objective_function = "CPM",
+#                                  seed = 233)
 cluster_df <- calculate_clusters(obj, 
                                  algorithm = "leiden", 
                                  resolution = 1, objective_function = "modularity",
                                  seed = 233)
-cluster_df <- calculate_clusters(obj, 
-                                 algorithm = "leiden", 
-                                 resolution = 1, objective_function = "modularity",
-                                 seed = 233)
+
 
 obj <- AddMetaData(object = obj, metadata = cluster_df)
 DimPlot(obj, group.by = "cluster")
 
+obj <- Seurat::SetIdent(obj, value = "cluster")
+ndims <- 50; reduction <- "pca"
+obj <- Seurat::RunUMAP(obj, dims = 1:ndims, reduction = reduction)
+DimPlot(obj, group.by = "cluster")
+
+# SCTransform
+obj <- SeuratObject::LoadSeuratRds( file.path(path_anal,"scratch","00_preprocessing_rds",paste0(library,".rdsSeurat")) )
+obj <- Seurat::SCTransform(obj)
+obj <- Seurat::RunPCA(obj)
+obj <- Seurat::FindNeighbors(obj, dims = 1:ndims, reduction = reduction)
+cluster_df <- calculate_clusters(obj, 
+                                 algorithm = "leiden", 
+                                 resolution = 1, objective_function = "modularity",
+                                 seed = 233)
+obj <- AddMetaData(object = obj, metadata = cluster_df)
+obj <- Seurat::SetIdent(obj, value = "cluster")
+obj <- Seurat::RunUMAP(obj, dims = 1:ndims, reduction = reduction)
+DimPlot(obj, group.by = "cluster")
+
+# SAM algorithm
+library(reticulate)
+use_condaenv("openscpca-cell-type-wilms-tumor-14")
+samalg <- import("samalg") #https://github.com/atarashansky/self-assembling-manifold/tree/master
+count_mat <- SeuratObject::GetAssayData(obj, assay = "RNA", layer = "count")
+count_mat <- t(count_mat)
+sam = samalg$SAM(counts = c(r_to_py(count_mat), 
+                            r_to_py(as.array(rownames(obj))),
+                            r_to_py(as.array(colnames(obj)))))
+sam$preprocess_data()
+sam$run(distance = 'correlation')
+sam$clustering(method = "leiden") #leiden clustering is the default algorithm in SAM
+sam$save_anndata(paste0("sam.",ind.library,".h5ad"))
+
 # anchor transfer results
-level = "compartment"
+level = "celltype"
 predictions <- read.csv(file.path(path_anal, "results", "01_anchor_transfer_seurat", paste0(library, "_", level,".csv")))
 obj <- AddMetaData(object = obj, metadata = predictions)
 plot_anchorTrans(path_anal = path_anal, 
